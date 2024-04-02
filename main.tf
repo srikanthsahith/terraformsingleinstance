40443c13aa88d31acc57ef7a01f25782ce75340e
srikanthsahith <165756868+srikanthsahith@users.noreply.github.com>
2024-04-02 17:23:23 +0530

    Code RKE Nodes

--- main.tf
+++ main.tf
@@ -1,18 +1,6 @@
 #This Terraform Code Deploys Basic VPC Infra.
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
     region = "${var.aws_region}"
-}
-
-terraform {
-  required_version = "<= 1.3.14" #Forcing which version of Terraform needs to be used
-  required_providers {
-    aws = {
-      version = "<= 5.0.0" #Forcing which version of plugin needs to be used.
-      source = "hashicorp/aws"
-    }
-  }
 }
 
 resource "aws_vpc" "default" {
@@ -109,7 +97,7 @@ resource "aws_security_group" "allow_all" {
 # }
 
 
-# resource "aws_instance" "web-1" {
+#resource "aws_instance" "web-1" {
 #     ami = var.imagename
 #     #ami = "ami-0d857ff0f5fc4e03b"
 #     #ami = "${data.aws_ami.my_ami.id}"
@@ -126,21 +114,3 @@ resource "aws_security_group" "allow_all" {
 # 	CostCenter = "ABCD"
 #     }
 # }
-
-##output "ami_id" {
-#  value = "${data.aws_ami.my_ami.id}"
-#}
-#!/bin/bash
-# echo "Listing the files in the repo."
-# ls -al
-# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-# echo "Running Packer Now...!!"
-# packer build -var=aws_access_key=AAAAAAAAAAAAAAAAAA -var=aws_secret_key=BBBBBBBBBBBBB packer.json
-#packer validate --var-file creds.json packer.json
-#packer build --var-file creds.json packer.json
-#packer.exe build --var-file creds.json -var=aws_access_key=AAAAAAAAAAAAAAAAAA -var=aws_secret_key=BBBBBBBBBBBBB packer.json
-# echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
-# echo "Running Terraform Now...!!"
-# terraform init
-# terraform apply --var-file terraform.tfvars -var="aws_access_key=AAAAAAAAAAAAAAAAAA" -var="aws_secret_key=BBBBBBBBBBBBB" --auto-approve
-#https://discuss.devopscube.com/t/how-to-get-the-ami-id-after-a-packer-build/36
